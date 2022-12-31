25c84e04262a4e11be1762e3d058d69d67d54647
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-01 02:43:22 +0530

    mapped admin doc routes

--- src/routes/Admin.ts
+++ src/routes/Admin.ts
@@ -1,11 +1,32 @@
 import { Router } from "express";
-import { getAllUsers, updateAccessLevel } from "../Controllers/Admin/User";
+import {
+  addMultipleStudents,
+  addStudent,
+  getStudents,
+  updateStudentDetails,
+} from "../Controllers/Admin/Docs";
+import {
+  deleteUser,
+  getAllUsers,
+  updateAccessLevel,
+} from "../Controllers/Admin/User";
 
 const router = Router();
 
-// manage users
+// user routes
 router.get("/all-users", getAllUsers);
 
-router.put("/edit-user", updateAccessLevel);
+router.put("/edit-access-level", updateAccessLevel);
+
+router.delete("/delete-user", deleteUser);
+
+// doc routes
+router.get("/all-students", getStudents);
+
+router.post("/create-student", addStudent);
+
+router.post("create-students", addMultipleStudents);
+
+router.put("/update-student", updateStudentDetails);
 
 export default router;

bd81dbbe01332e840bee3a378e63713231867292
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-01 02:12:22 +0530

    dept model

---
+++ src/models/Dept.ts
@@ -0,0 +1,20 @@
+import { model, Schema } from "mongoose";
+import { IDept } from "../types/DVA";
+
+const DeptSchema = new Schema({
+  code: {
+    type: String,
+    required: true,
+    unique: true,
+    min: 3,
+    max: 10,
+  },
+  name: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 50,
+  },
+});
+
+export default model<IDept>("Dept", DeptSchema);

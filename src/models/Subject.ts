cab203e1b7fc9d37b57d5c6613481423242bb954
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-01 02:12:38 +0530

    subject model

---
+++ src/models/Subject.ts
@@ -0,0 +1,24 @@
+import { model, Schema } from "mongoose";
+import { ISubject } from "../types/DVA";
+
+const SubjectSchema = new Schema({
+  code: {
+    type: String,
+    required: true,
+    unique: true,
+    min: 3,
+    max: 10,
+  },
+  name: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 50,
+  },
+  credits: {
+    type: Number,
+    required: true,
+  },
+});
+
+export default model<ISubject>("Subject", SubjectSchema);

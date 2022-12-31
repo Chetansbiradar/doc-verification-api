be1c728680162b3f52743cd0c172d6f39988b125
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-01 02:13:07 +0530

    student model

---
+++ src/models/Student.ts
@@ -0,0 +1,40 @@
+import { model, Schema } from "mongoose";
+import { IStudent } from "../types/DVA";
+
+const StudentSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    max: 50,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+    max: 50,
+  },
+  department: {
+    type: Schema.Types.ObjectId,
+    ref: "Dept",
+    required: true,
+  },
+  batch: {
+    type: String,
+    required: true,
+  },
+  reportCards: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "ReportCard",
+    },
+  ],
+});
+
+export default model<IStudent>("Student", StudentSchema);

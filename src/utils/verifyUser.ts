30b4783a23b3a5d714b3ae55cb91dcbbaab30952
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-01 01:59:22 +0530

    sending user doc in req from verify user to verify admin

--- src/utils/verifyUser.ts
+++ src/utils/verifyUser.ts
@@ -1,29 +1,37 @@
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import User from "../models/User";
 
-export default (req: Request, res: Response, next: NextFunction) => {
+export default async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const token: string = req.cookies.token;
     if (!token) throw Error("No token found");
 
     const secret: string = process.env.ACCESS_TOKEN_SEC as string;
 
-    const { _id, accessLevel } = jwt.verify(token, secret) as {
+    const { _id } = jwt.verify(token, secret) as {
       _id: string;
-      accessLevel: number;
     };
 
-    const user = User.findById(_id);
+    const user = await User.findById(_id);
     if (!user) throw Error("User does not exist");
 
-    req.body.userId = { _id, accessLevel };
+    req.body.user = {
+      _id,
+      name: user.name,
+      email: user.email,
+      accessLevel: user.accessLevel,
+    };
 
     next();
   } catch (err) {
     res.status(401).json({
       success: false,
-      message: "Unauthorized" + (err as Error).message,
+      message: "Unauthorized " + (err as Error).message,
     });
   }
 };

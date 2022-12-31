1d4d631c2732f0c18e17ec20f312a5556e6bb4cc
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-01 02:04:14 +0530

    model interface types

--- src/types/DVA.d.ts
+++ src/types/DVA.d.ts
@@ -1,8 +1,51 @@
 import { Document } from "mongoose";
 
 export interface IUser extends Document {
+  _id: string;
   name: string;
   email: string;
   accessLevel: number;
   password: string;
+}
+
+export interface IDept extends Document {
+  _id: string;
+  code: string;
+  name: string;
+}
+
+export interface ISubject extends Document {
+  _id: string;
+  code: string;
+  name: string;
+  credits: number;
+}
+
+export interface IExam extends Document {
+  _id: string;
+  name: string;
+  subjects: string[];
+  MMYYofExam: string;
+  semester: number;
+}
+
+export interface ReportCard extends Document {
+  _id: string;
+  student: string;
+  department: string;
+  exam: string;
+  grades: {
+    subject: string;
+    grade: string;
+  }[];
+}
+
+export interface IStudent extends Document {
+  _id: string;
+  name: string;
+  email: string;
+  phone: string;
+  department: string;
+  batch: string;
+  reportCards: string[];
 }

7b4931e3e111fc958529944f6271c0ee081e096b
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-01 16:00:57 +0530

    mapping admin routes and controllers

--- src/routes/Admin.ts
+++ src/routes/Admin.ts
@@ -1,15 +1,25 @@
 import { Router } from "express";
+import multer from "multer";
 import {
-  addMultipleStudents,
+  addReport,
   addStudent,
   getStudents,
   updateStudentDetails,
 } from "../Controllers/Admin/Docs";
+import { addDept } from "../Controllers/Admin/Management";
 import {
   deleteUser,
   getAllUsers,
   updateAccessLevel,
 } from "../Controllers/Admin/User";
+
+const maxFileSize = 20;
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: maxFileSize * 1024 * 1024, // no larger than 5mb
+  },
+});
 
 const router = Router();
 
@@ -25,8 +35,13 @@ router.get("/all-students", getStudents);
 
 router.post("/create-student", addStudent);
 
-router.post("create-students", addMultipleStudents);
-
 router.put("/update-student", updateStudentDetails);
+
+router.put("/add-report", upload.single("file"), addReport);
+
+// router.post("create-students", addStudents);
+
+// management routes
+router.post("/create-dept", addDept);
 
 export default router;

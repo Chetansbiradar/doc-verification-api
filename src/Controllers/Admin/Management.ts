64c0e330e231bea3e080f271823f75d91613ec3e
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-08 21:57:07 +0530

    crud depts

--- src/Controllers/Admin/Management.ts
+++ src/Controllers/Admin/Management.ts
@@ -2,6 +2,24 @@ import { Request, Response } from "express";
 import Dept from "../../models/Dept";
 import { IDept } from "../../types/DVA";
 
+const getDepts = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const depts: IDept[] = await Dept.find();
+    if (!depts) throw new Error("No departments found");
+
+    res.status(200).json({
+      success: true,
+      message: "Returning all departments",
+      depts,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+};
+
 const addDept = async (req: Request, res: Response): Promise<void> => {
   try {
     const { code, name } = req.body as Pick<IDept, "code" | "name">;
@@ -18,8 +36,7 @@ const addDept = async (req: Request, res: Response): Promise<void> => {
 
     res.status(200).json({
       success: true,
-      message: "Department added successfully",
-      dept: savedDept,
+      message: "Department added",
     });
   } catch (error) {
     res.status(500).json({
@@ -29,4 +46,49 @@ const addDept = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
-export { addDept };
+const editDept = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { code, name } = req.body as Pick<IDept, "code" | "name">;
+    if (!req.body.deptId || !name || !code)
+      throw new Error("Please fill out all fields");
+
+    const updatedDept: IDept | null = await Dept.findByIdAndUpdate(
+      req.body.deptId,
+      { code, name },
+      { new: true }
+    );
+    if (!updatedDept) throw new Error("Department update failed");
+
+    res.status(200).json({
+      success: true,
+      message: "Department updated",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+};
+
+const deleteDept = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { deptId } = req.body;
+    if (!deptId) throw new Error("Please fill out all fields");
+
+    const deletedDept: IDept | null = await Dept.findByIdAndDelete(deptId);
+    if (!deletedDept) throw new Error("Department delete failed");
+
+    res.status(200).json({
+      success: true,
+      message: "Department deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+};
+
+export { getDepts, addDept, editDept, deleteDept };

14eb8afd3b6a1a96c873067eefec6690630d93d7
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-12 01:38:38 +0530

    mgmnt controllers for admin

--- src/Controllers/Admin/Management.ts
+++ src/Controllers/Admin/Management.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import Dept from "../../models/Dept";
-import { IDept } from "../../types/DVA";
+import Exam from "../../models/Exam";
+import Subject from "../../models/Subject";
+import { IDept, IExam, ISubject } from "../../types/DVA";
 
 const getDepts = async (req: Request, res: Response): Promise<void> => {
   try {
@@ -91,4 +93,214 @@ const deleteDept = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
-export { getDepts, addDept, editDept, deleteDept };
+const getSubjects = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const subjects: ISubject[] = await Subject.find();
+    if (!subjects) throw new Error("No subjects found");
+
+    res.status(200).json({
+      success: true,
+      message: "Returning all subjects",
+      subjects,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+};
+
+const addSubject = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { code, name, credits } = req.body as Pick<
+      ISubject,
+      "code" | "name" | "credits"
+    >;
+    if (!code || !name || !credits)
+      throw new Error("Please fill out all fields");
+
+    const subject: ISubject | null = await Subject.findOne({ code });
+    if (subject) throw new Error("Subject already exists");
+
+    const newSubject: ISubject = new Subject({ code, name, credits });
+
+    const savedSubject: ISubject = await newSubject.save();
+    if (!savedSubject)
+      throw new Error("Something went wrong saving the subject");
+
+    res.status(200).json({
+      success: true,
+      message: "Subject added",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+};
+
+const editSubject = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { code, name, credits } = req.body as Pick<
+      ISubject,
+      "code" | "name" | "credits"
+    >;
+    if (!req.body.subjectId || !code || !name || !credits)
+      throw new Error("Please fill out all fields");
+
+    const updatedSubject: ISubject | null = await Subject.findByIdAndUpdate(
+      req.body.subjectId,
+      { code, name, credits },
+      { new: true }
+    );
+
+    if (!updatedSubject) throw new Error("Subject update failed");
+
+    res.status(200).json({
+      success: true,
+      message: "Subject updated",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+};
+
+const deleteSubject = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { subjectId } = req.body;
+    if (!subjectId) throw new Error("Please fill out all fields");
+
+    const deletedSubject: ISubject | null = await Subject.findByIdAndDelete(
+      subjectId
+    );
+    if (!deletedSubject) throw new Error("Subject delete failed");
+
+    res.status(200).json({
+      success: true,
+      message: "Subject deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+};
+
+const getExams = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const exams: IExam[] = await Exam.find()
+      .populate("subjects")
+      .populate("dept");
+    if (!exams) throw new Error("No exams found");
+
+    res.status(200).json({
+      success: true,
+      message: "Returning all exams",
+      exams,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+};
+
+const addExam = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { dept, subjects, examDate, semester } = req.body as Pick<
+      IExam,
+      "dept" | "subjects" | "examDate" | "semester"
+    >;
+    if (!dept || !subjects || !examDate || !semester)
+      throw new Error("Please fill out all fields");
+
+    const exam: IExam | null = await Exam.findOne({ examDate, dept, semester });
+    if (exam) throw new Error("Exam already exists");
+
+    const newExam: IExam = new Exam({ dept, subjects, examDate, semester });
+
+    const savedExam: IExam = await newExam.save();
+    if (!savedExam) throw new Error("Something went wrong saving the exam");
+
+    res.status(200).json({
+      success: true,
+      message: "Exam added",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+};
+
+const editExam = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { dept, subjects, examDate, semester } = req.body as Pick<
+      IExam,
+      "dept" | "subjects" | "examDate" | "semester"
+    >;
+    if (!req.body.examId || !dept || !subjects || !examDate || !semester)
+      throw new Error("Please fill out all fields");
+
+    const updatedExam: IExam | null = await Exam.findByIdAndUpdate(
+      req.body.examId,
+      { dept, subjects, examDate, semester },
+      { new: true }
+    );
+
+    if (!updatedExam) throw new Error("Exam update failed");
+
+    res.status(200).json({
+      success: true,
+      message: "Exam updated",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+};
+
+const deleteExam = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { examId } = req.body;
+    if (!examId) throw new Error("Please fill out all fields");
+
+    const deletedExam: IExam | null = await Exam.findByIdAndDelete(examId);
+    if (!deletedExam) throw new Error("Exam delete failed");
+
+    res.status(200).json({
+      success: true,
+      message: "Exam deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+};
+
+export {
+  getDepts,
+  addDept,
+  editDept,
+  deleteDept,
+  getSubjects,
+  addSubject,
+  editSubject,
+  deleteSubject,
+  getExams,
+  addExam,
+  editExam,
+  deleteExam,
+};

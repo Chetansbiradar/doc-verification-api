d579d39520b18f6c6265d7388830b98dc18c1ac3
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-12 01:38:51 +0530

    user controllers

--- src/Controllers/Admin/User.ts
+++ src/Controllers/Admin/User.ts
@@ -21,17 +21,102 @@ const getAllUsers = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
+const getVerifiers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const users: IUser[] = await User.find({ accessLevel: 1 })
+      .select("-password")
+      .populate({
+        path: "canAccess",
+        populate: {
+          path: "department",
+        },
+      });
+
+    if (users?.length === 0) {
+      res.status(200).json({
+        success: true,
+        message: "No verifiers found",
+        users: [],
+      });
+      return;
+    }
+    if (!users) throw new Error("No users found");
+
+    res.status(200).json({
+      success: true,
+      message: "Returning all verifiers",
+      users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+};
+
 const updateAccessLevel = async (
   req: Request,
   res: Response
 ): Promise<void> => {
   try {
     const { userId, accessLevel } = req.body;
-    if (!userId || isNaN(accessLevel)) throw new Error("Please fill out all fields");
+    if (!userId || isNaN(accessLevel))
+      throw new Error("Please fill out all fields");
 
     const updatedUser: IUser | null = await User.findByIdAndUpdate(
       userId,
       { accessLevel },
+      { new: true }
+    ).select("-password");
+    if (!updatedUser) throw new Error("User update failed");
+
+    res.status(200).json({
+      success: true,
+      message: "User updated",
+      user: updatedUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+};
+
+const addAccess = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { uid, sids } = req.body;
+    if (!uid || !sids) throw new Error("Please fill out all fields");
+
+    const updatedUser: IUser | null = await User.findByIdAndUpdate(
+      uid,
+      { $push: { canAccess: { $each: sids } } },
+      { new: true }
+    ).select("-password");
+    if (!updatedUser) throw new Error("User update failed");
+
+    res.status(200).json({
+      success: true,
+      message: "User updated",
+      user: updatedUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    });
+  }
+};
+
+const removeAccess = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { uid, sid } = req.body;
+    if (!uid || !sid) throw new Error("Please fill out all fields");
+
+    const updatedUser: IUser | null = await User.findByIdAndUpdate(
+      uid,
+      { $pull: { canAccess: sid } },
       { new: true }
     ).select("-password");
     if (!updatedUser) throw new Error("User update failed");
@@ -72,4 +157,11 @@ const deleteUser = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
-export { getAllUsers, updateAccessLevel, deleteUser };
+export {
+  getAllUsers,
+  getVerifiers,
+  updateAccessLevel,
+  deleteUser,
+  addAccess,
+  removeAccess,
+};
